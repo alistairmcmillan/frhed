cd8c8892e816ba50d77e7f6d5246afe1f3747315
Kimmo Varis <kimmov@gmail.com>
2008-08-21 19:36:04 +0000

    Whitespace cleanup. 'interface' is not a C++ keyword, use 'class' instead.

--- FRHED/ido.h
+++ FRHED/ido.h
@@ -1,75 +1,102 @@
+/////////////////////////////////////////////////////////////////////////////
+//    License (GPLv2+):
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful, but
+//    WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/////////////////////////////////////////////////////////////////////////////
+/** 
+ * @file  ido.h
+ *
+ * @brief Declarations of clipboard data handling classes.
+ *
+ */
+// ID line follows -- this is updated by SVN
+// $Id$
+
 #ifndef ido_h
 #define ido_h
 
-
-struct DataSpecifier{
+struct DataSpecifier
+{
 	STGMEDIUM medium;
 	FORMATETC format;
 };
 
-interface CDataObject: public IDataObject
+class CEnumFORMATETC;
+
+class CDataObject: public IDataObject
 {
-	friend interface CEnumFORMATETC;
-	private:
-		ULONG m_cRefCount;
-		bool deleteself;
-		CDataObject** pthis;
+	friend CEnumFORMATETC;
+private:
+	ULONG m_cRefCount;
+	bool deleteself;
+	CDataObject** pthis;
 
-		bool allowSetData;      //Allow the IDataObject::SetData call
-		DataSpecifier* data;    //Pointer to an array of STGMEDIUM+FORMATETC pairs
-		unsigned int numdata;   //Number of data objects in data
-		CEnumFORMATETC** enums; //An array of pointers to IEnumFORMATETC objects
-		unsigned int numenums;  //Number of pointers in enums
+	bool allowSetData;      //Allow the IDataObject::SetData call
+	DataSpecifier* data;    //Pointer to an array of STGMEDIUM+FORMATETC pairs
+	unsigned int numdata;   //Number of data objects in data
+	CEnumFORMATETC** enums; //An array of pointers to IEnumFORMATETC objects
+	unsigned int numenums;  //Number of pointers in enums
 
-	public:
-		//Members
-		CDataObject( bool delself = false, CDataObject** p = NULL );
-		~CDataObject( void );
-		void DisableSetData( void );
-		void Empty( void );
+public:
+	//Members
+	CDataObject( bool delself = false, CDataObject** p = NULL );
+	~CDataObject( void );
+	void DisableSetData( void );
+	void Empty( void );
 
-		//IUnknown members
-		STDMETHODIMP QueryInterface( REFIID iid, void** ppvObject );
-		STDMETHODIMP_(ULONG) AddRef( void );
-		STDMETHODIMP_(ULONG) Release( void );
+	//IUnknown members
+	STDMETHODIMP QueryInterface( REFIID iid, void** ppvObject );
+	STDMETHODIMP_(ULONG) AddRef( void );
+	STDMETHODIMP_(ULONG) Release( void );
 
-		//IDataObject members
-		STDMETHODIMP GetData( FORMATETC* pFormatetc, STGMEDIUM* pmedium );
-		STDMETHODIMP GetDataHere( FORMATETC* pFormatetc, STGMEDIUM* pmedium );
-		STDMETHODIMP QueryGetData( FORMATETC* pFormatetc );
-		STDMETHODIMP GetCanonicalFormatEtc( FORMATETC* pFormatetcIn, FORMATETC* pFormatetcOut );
-		STDMETHODIMP SetData( FORMATETC* pFormatetc, STGMEDIUM* pmedium, BOOL fRelease );
-		STDMETHODIMP EnumFormatEtc( DWORD dwDirection, IEnumFORMATETC** ppenumFormatetc );
-		STDMETHODIMP DAdvise( FORMATETC* pFormatetc, DWORD advf, IAdviseSink* pAdvSink, DWORD* pdwConnection );
-		STDMETHODIMP DUnadvise( DWORD dwConnection );
-		STDMETHODIMP EnumDAdvise( IEnumSTATDATA** ppenumAdvise );
+	//IDataObject members
+	STDMETHODIMP GetData( FORMATETC* pFormatetc, STGMEDIUM* pmedium );
+	STDMETHODIMP GetDataHere( FORMATETC* pFormatetc, STGMEDIUM* pmedium );
+	STDMETHODIMP QueryGetData( FORMATETC* pFormatetc );
+	STDMETHODIMP GetCanonicalFormatEtc( FORMATETC* pFormatetcIn, FORMATETC* pFormatetcOut );
+	STDMETHODIMP SetData( FORMATETC* pFormatetc, STGMEDIUM* pmedium, BOOL fRelease );
+	STDMETHODIMP EnumFormatEtc( DWORD dwDirection, IEnumFORMATETC** ppenumFormatetc );
+	STDMETHODIMP DAdvise( FORMATETC* pFormatetc, DWORD advf, IAdviseSink* pAdvSink, DWORD* pdwConnection );
+	STDMETHODIMP DUnadvise( DWORD dwConnection );
+	STDMETHODIMP EnumDAdvise( IEnumSTATDATA** ppenumAdvise );
 };
 
-interface CEnumFORMATETC : public IEnumFORMATETC
+class CEnumFORMATETC : public IEnumFORMATETC
 {
-	private:
-		DWORD m_cRefCount;
-		bool deleteself;
-		CEnumFORMATETC** pthis;
+private:
+	DWORD m_cRefCount;
+	bool deleteself;
+	CEnumFORMATETC** pthis;
 
-		CDataObject* parent; //Pointer to the parent IDataObject
-		unsigned int index;  //The index of the data in the parent's data storage
+	CDataObject* parent; //Pointer to the parent IDataObject
+	unsigned int index;  //The index of the data in the parent's data storage
 
-	public:
-		//Members
-		CEnumFORMATETC( CDataObject*par, bool delself = false, CEnumFORMATETC** p = NULL );
-		~CEnumFORMATETC( void );
+public:
+	//Members
+	CEnumFORMATETC( CDataObject*par, bool delself = false, CEnumFORMATETC** p = NULL );
+	~CEnumFORMATETC( void );
 
-		//IUnknown members
-		STDMETHODIMP QueryInterface( REFIID iid, void** ppvObject );
-		STDMETHODIMP_(ULONG) AddRef( void );
-		STDMETHODIMP_(ULONG) Release( void );
+	//IUnknown members
+	STDMETHODIMP QueryInterface( REFIID iid, void** ppvObject );
+	STDMETHODIMP_(ULONG) AddRef( void );
+	STDMETHODIMP_(ULONG) Release( void );
 
-		//IEnumFORMATETC members
-		STDMETHODIMP Next( ULONG celt, FORMATETC* rgelt, ULONG* pceltFetched );
-		STDMETHODIMP Skip( ULONG celt );
-		STDMETHODIMP Reset( void );
-		STDMETHODIMP Clone( IEnumFORMATETC** ppenum );
+	//IEnumFORMATETC members
+	STDMETHODIMP Next( ULONG celt, FORMATETC* rgelt, ULONG* pceltFetched );
+	STDMETHODIMP Skip( ULONG celt );
+	STDMETHODIMP Reset( void );
+	STDMETHODIMP Clone( IEnumFORMATETC** ppenum );
 };
 
 #endif // ido_h
--- FRHED/ids.h
+++ FRHED/ids.h
@@ -1,27 +1,51 @@
+/////////////////////////////////////////////////////////////////////////////
+//    License (GPLv2+):
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful, but
+//    WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/////////////////////////////////////////////////////////////////////////////
+/** 
+ * @file  ids.h
+ *
+ * @brief Declaration of (Drag &) Drop data source class.
+ *
+ */
+// ID line follows -- this is updated by SVN
+// $Id$
+
 #ifndef ids_h
 #define ids_h
 
-
-interface CDropSource: public IDropSource
+class CDropSource: public IDropSource
 {
-	private:
-		ULONG m_cRefCount;
-		bool deleteself;
-		CDropSource** pthis;
+private:
+	ULONG m_cRefCount;
+	bool deleteself;
+	CDropSource** pthis;
 
-	public:
-		//Members
-		CDropSource( bool delself = false, CDropSource** p = NULL);
-		~CDropSource( void );
+public:
+	//Members
+	CDropSource( bool delself = false, CDropSource** p = NULL);
+	~CDropSource( void );
 
-		//IUnknown members
-		STDMETHODIMP QueryInterface( REFIID iid, void** ppvObject );
-		STDMETHODIMP_(ULONG) AddRef( void );
-		STDMETHODIMP_(ULONG) Release( void );
+	//IUnknown members
+	STDMETHODIMP QueryInterface( REFIID iid, void** ppvObject );
+	STDMETHODIMP_(ULONG) AddRef( void );
+	STDMETHODIMP_(ULONG) Release( void );
 
-		//IDataObject members
-		STDMETHODIMP GiveFeedback( DWORD dwEffect );
-		STDMETHODIMP QueryContinueDrag( BOOL fEscapePressed,DWORD grfKeyState );
+	//IDataObject members
+	STDMETHODIMP GiveFeedback( DWORD dwEffect );
+	STDMETHODIMP QueryContinueDrag( BOOL fEscapePressed,DWORD grfKeyState );
 };
 
 
