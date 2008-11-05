a9c1623ef0861f27b7925ef7306adc52d0941b29
Tim Gerundt <tim@gerundt.de>
2008-11-05 23:34:02 +0000

    PATCH: [ 2208460 ] Init DocBook Manual

---
+++ Docs/Users/Manual/build/build_html.bat
@@ -0,0 +1,26 @@
+@echo off
+
+call configuration.bat
+
+set docbook_inputfile=..\Frhed_Help.xml
+set docbook_use_stylesheet=build_html.xsl
+set docbook_outputdir=%docbook_build_path%\html
+
+if not exist "%docbook_outputdir%" mkdir "%docbook_outputdir%"
+
+echo Copy screenshots...
+if not exist "%docbook_outputdir%\screenshots" mkdir "%docbook_outputdir%\screenshots"
+copy "..\screenshots\*.*" "%docbook_outputdir%\screenshots\."
+
+echo Copy stylesheets...
+if not exist "%docbook_outputdir%\css" mkdir "%docbook_outputdir%\css"
+copy "..\css\all.css" "%docbook_outputdir%\css\all.css"
+copy "..\css\print.css" "%docbook_outputdir%\css\print.css"
+
+echo Create HTML files...
+%docbook_java_exe% %docbook_java_parameters% -cp %docbook_saxon_jar%;%docbook_xerces_jar%;%docbook_saxon_xsl% -Djavax.xml.parsers.DocumentBuilderFactory=%DBFACTORY% -Djavax.xml.parsers.SAXParserFactory=%SPFACTORY% -Dorg.apache.xerces.xni.parser.XMLParserConfiguration=%XINCLUDE% com.icl.saxon.StyleSheet %docbook_inputfile% %docbook_use_stylesheet% base.dir=%docbook_outputdir%\
+
+echo Finished!
+
+:end
+@echo on
---
+++ Docs/Users/Manual/build/build_html.xsl
@@ -0,0 +1,21 @@
+<?xml version="1.0"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+		version="1.0">
+
+<!-- ********************************************************************
+     build_html.xsl
+     WinMerge customization of the XSL DocBook stylesheet distribution.
+     Contains customized templates and parameters for the HTML version 
+     of help; all other styles are inherited from the WinMerge 
+     build_common.xsl and the stock DocBook stylesheets in ../build/xsl. 
+ 
+     See build_common.xsl for editing instructions.
+     ******************************************************************** 
+     Changes:
+     Created July 2008
+     ******************************************************************** -->
+
+<xsl:import href="../build/xsl/html/chunk.xsl"/>
+<xsl:import href="build_common.xsl"/>
+
+</xsl:stylesheet>
