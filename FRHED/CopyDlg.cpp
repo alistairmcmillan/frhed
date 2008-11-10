b821303555d2f71a58fc47c3fb691f793792900e
Kimmo Varis <kimmov@gmail.com>
2008-11-10 22:23:05 +0000

    Make string parsing in the Copy dialog safer.

--- FRHED/CopyDlg.cpp
+++ FRHED/CopyDlg.cpp
@@ -1,3 +1,28 @@
+/////////////////////////////////////////////////////////////////////////////
+//    License (GPLv2+):
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful, but
+//    WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/////////////////////////////////////////////////////////////////////////////
+/** 
+ * @file  CopyDlg.cpp
+ *
+ * @brief Copy dialog implementation.
+ *
+ */
+// ID line follows -- this is updated by SVN
+// $Id$
+
 #include "precomp.h"
 #include "resource.h"
 #include "hexwnd.h"
@@ -8,10 +33,10 @@ BOOL CopyDlg::OnInitDialog(HWND hDlg)
 {
 	int iStart = iGetStartOfSelection();
 	int iEnd = iGetEndOfSelection();
-	char buf[32];
-	sprintf(buf, "x%x", iStart);
+	char buf[32] = {0};
+	_snprintf(buf, RTL_NUMBER_OF(buf) - 1, "x%x", iStart);
 	SetDlgItemText(hDlg, IDC_EDIT1, buf);
-	sprintf(buf, "x%x", iEnd);
+	_snprintf(buf, RTL_NUMBER_OF(buf) - 1, "x%x", iEnd);
 	SetDlgItemText(hDlg, IDC_EDIT2, buf);
 	SetDlgItemInt(hDlg, IDC_EDIT3, iEnd - iStart + 1, TRUE);
 	CheckDlgButton(hDlg, IDC_RADIO1, BST_CHECKED);
@@ -20,7 +45,7 @@ BOOL CopyDlg::OnInitDialog(HWND hDlg)
 
 BOOL CopyDlg::Apply(HWND hDlg)
 {
-	char buf[64];
+	char buf[64] = {0};
 	int iOffset;
 	int iNumberOfBytes;
 	if (GetDlgItemText(hDlg, IDC_EDIT1, buf, 64) &&
