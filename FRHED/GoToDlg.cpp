9a278805537ec6a4309cd1e3820a5e96a600e6d6
Kimmo Varis <kimmov@gmail.com>
2008-12-04 23:13:41 +0000

    Fix control ID in Goto dialog.

--- FRHED/GoToDlg.cpp
+++ FRHED/GoToDlg.cpp
@@ -1,3 +1,28 @@
+/////////////////////////////////////////////////////////////////////////////
+//    License (GPLv2+):
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful, but
+//    WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/////////////////////////////////////////////////////////////////////////////
+/** 
+ * @file  GotoDlg.cpp
+ *
+ * @brief Implementation of the Go To-dialog.
+ *
+ */
+// ID line follows -- this is updated by SVN
+// $Id$
+
 #include "precomp.h"
 #include "resource.h"
 #include "hexwnd.h"
@@ -8,7 +33,7 @@ char GoToDlg::buffer[16];
 BOOL GoToDlg::Apply(HWND hDlg)
 {
 	int offset, i = 0, r = 0;
-	GetDlgItemText(hDlg, IDC_EDIT1, buffer, sizeof buffer);
+	GetDlgItemText(hDlg, IDC_GOTO_OFFSET, buffer, sizeof buffer);
 	// For a relative jump, read offset from 2nd character on.
 	if (buffer[0] == '+' || buffer[0] == '-')
 		r = 1;
@@ -42,7 +67,7 @@ INT_PTR GoToDlg::DlgProc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	switch (iMsg)
 	{
 	case WM_INITDIALOG:
-		SetDlgItemText(hDlg, IDC_EDIT1, buffer);
+		SetDlgItemText(hDlg, IDC_GOTO_OFFSET, buffer);
 		return TRUE;
 	case WM_COMMAND:
 		switch (wParam)
--- FRHED/resource.h
+++ FRHED/resource.h
@@ -240,6 +240,7 @@
 #define IDC_ABOUTCONTRIBS               324
 #define IDC_ABOUT_URL                   325
 #define IDC_ABOUT_OPENURL               326
+#define IDC_GOTO_OFFSET                 327
 #define IDC_ENABLE_DRAG                 1074
 #define IDC_ALWAYS_CHOOSE               1075
 #define IDC_DROP_BIN_DATA               1076
