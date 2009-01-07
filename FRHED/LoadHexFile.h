45b545c149f37761972bfb07708b3f78387348a9
Kimmo Varis <kimmov@gmail.com>
2009-01-07 12:04:39 +0000

    Add doxygen comments.

--- FRHED/LoadHexFile.h
+++ FRHED/LoadHexFile.h
@@ -28,6 +28,9 @@
 
 #include "Simparr.h"
 
+/**
+ * @brief Hex data streaming class.
+ */
 class hexfile_stream
 {
 public:
@@ -40,69 +43,135 @@ public:
 	int lheatwhite();
 };
 
+/**
+ * @brief Hex file streaming class.
+ */
 class fhexfile_stream : public hexfile_stream
 {
+private:
+	FILE *m_hexfile; /**< File pointer to the hex file. */
+
 public:
-	// Data
-	FILE *i;
-	// Constructor
-	fhexfile_stream(FILE *i): i(i) { }
-	// Methods
+	/** 
+	 * Constructor.
+	 * @param [in] hexfile file pointer to open file.
+	 */
+	fhexfile_stream(FILE *hexfile): m_hexfile(hexfile) { }
+
+	/**
+	 * Get next character from the file.
+	 */
 	virtual int lhgetc()
 	{
-		return fgetc(i);
+		return fgetc(m_hexfile);
 	}
+	
+	/**
+	 * Return character to the file.
+	 * @param [in] c Character to put back to the file.
+	 */
 	virtual int lhungetc(int c)
 	{
-		return ungetc(c, i);
+		return ungetc(c, m_hexfile);
 	}
+
+	/**
+	 * Gets current position in the file.
+	 * @return Current position.
+	 */
 	virtual long lhtell()
 	{
-		return ftell(i);
+		return ftell(m_hexfile);
 	}
+
+	/**
+	 * Move to given position in the file.
+	 * @param [in] p Position to move to.
+	 * @return 0 if success, != 0 otherwise.
+	 */
 	virtual int lhseek(long p)
 	{
-		return fseek(i, p, SEEK_SET);
+		return fseek(m_hexfile, p, SEEK_SET);
 	}
+
+	/**
+	 * Read formatted integer from file.
+	 * @param [in] fmt Format string.
+	 * @param [in] p Pointer where to put the read integer.
+	 * @return Count of read integers.
+	 */
 	virtual int scanf(const char *fmt, int *p)
 	{
-		return fscanf(i, fmt, p);
+		return fscanf(m_hexfile, fmt, p);
 	}
 };
 
+/**
+ * @brief Hex buffer streaming class.
+ */
 class chexfile_stream : public hexfile_stream
 {
+private:
+	char *m_pBuf; /**< Start address of the hex data buffer. */
+	long m_offset; /**< Offset in the buffer. */
+
 public:
-	// Data
-	char *i;
-	long lhpos;
-	// Constructor
-	chexfile_stream(char *i): i(i), lhpos(0) { }
-	// Methods
+	/** 
+	 * Constructor.
+	 * @param [in] pBuffer Pointer to begin of read buffer.
+	 */
+	chexfile_stream(char *pBuffer): m_pBuf(pBuffer), m_offset(0) { }
+
+	/**
+	 * Get next character from the buffer.
+	 */
 	virtual int lhgetc()
 	{
-		int c = (int)(BYTE)i[lhpos];
+		int c = (int)(BYTE)m_pBuf[m_offset];
 		if (c)
-			++lhpos;
+			++m_offset;
 		else
 			c = EOF;
 		return c;
 	}
+
+	/**
+	 * Return character to the buffer.
+	 * @param [in] c Character to put back to the buffer.
+	 */
 	virtual int lhungetc(int c)
 	{
-		return BYTE(i[--lhpos] = (BYTE)c);
+		return BYTE(m_pBuf[--m_offset] = (BYTE)c);
 	}
+
+	/**
+	 * Gets current position in the buffer.
+	 * @return Current position.
+	 */
 	virtual long lhtell()
 	{
-		return lhpos;
+		return m_offset;
 	}
+
+	/**
+	 * Move to given position in the buffer.
+	 * @param [in] p Position to move to.
+	 * @return 0 if success, != 0 otherwise.
+	 */
 	virtual int lhseek(long p)
 	{
-		return lhpos = p;
+		return m_offset = p;
 	}
+
+	/**
+	 * Read formatted integer from buffer.
+	 * @param [in] fmt Format string.
+	 * @param [in] p Pointer where to put the read integer.
+	 * @return Count of read integers.
+	 */
 	virtual int scanf(const char *fmt, int *p)
 	{
-		return sscanf(i + lhpos, fmt, p);
+		return sscanf(m_pBuf + m_offset, fmt, p);
 	}
 };
 
@@ -110,11 +179,13 @@ class load_hexfile_0 : SimpleArray<unsigned char>
 {
 private:
 	HWND hwnd;
+
 	load_hexfile_0(HexEditorWindow &hexwnd)
 	: hwnd(hexwnd.hwnd)
 	{
 	}
 	bool StreamIn(hexfile_stream &);
+
 public:
 	static bool StreamIn(HexEditorWindow &, hexfile_stream &);
 };
@@ -130,6 +201,7 @@ private:
 	int iCharacterSet;
 	int bPartialStats;
 	int iPartialOffset;
+
 	load_hexfile_1(HexEditorWindow &hexwnd)
 	: hwnd(hexwnd.hwnd)
 	, iMinOffsetLen(hexwnd.iMinOffsetLen)
@@ -142,6 +214,7 @@ private:
 	{
 	}
 	bool StreamIn(hexfile_stream &);
+
 public:
 	static bool StreamIn(HexEditorWindow &, hexfile_stream &);
 };
