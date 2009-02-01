b28a431cf439cca099cdfd6ed919e3039a5416ee
Tim Gerundt <tim@gerundt.de>
2009-02-01 12:43:18 +0000

    PATCH: [ 2553900 ] Web: Add screenshot page

---
+++ Web/screenshots/index.php
@@ -0,0 +1,16 @@
+<?php
+  include('../engine/page.inc');
+  
+  $page = new Page;
+  $page->printHead('Screenshots - Frhed');
+?>
+<h1 class="title">Frhed</h1>
+<p class="olive"><b>Fr</b>ee <b>h</b>ex <b>ed</b>itor</p>
+
+<h2>Screenshots</h2>
+<h3>Main Window</h3>
+<a href="frhed_main_window.png" target="_blank"><img src="frhed_main_window_small.png" alt="Main Window" style="border:none;" /></a>
+<p>The main window from Frhed.</p>
+<?php
+  $page->printFoot();
+?>
