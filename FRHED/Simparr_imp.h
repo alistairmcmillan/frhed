587668aee438c4c6fde2b26577ed4de8ad8575cf
Kimmo Varis <kimmov@gmail.com>
2009-02-01 21:18:29 +0000

    PATCH: [ 2552896 ] Fix crash trying to partially open large file

--- FRHED/Simparr_imp.h
+++ FRHED/Simparr_imp.h
@@ -335,9 +335,18 @@ template<class T> int SimpleArray<T>::SetSize(int nNewSize, int nGrowBy )
 		return TRUE;
 	}
 
-	T* pT = new T[nNewSize];
+	T* pT = NULL;
+	try
+	{
+		pT = new T[nNewSize];
+	}
+	catch (...)
+	{
+		return FALSE;
+	}
 	if (pT == NULL)
 		return FALSE;
+
 	int i;
 	if(m_nUpperBound < nNewSize)
 	{
