6562b362b6964732cfcb36e5523012017e61b1e1
Kimmo Varis <kimmov@gmail.com>
2009-04-02 21:28:11 +0000

    Build and copy language files to Languages subfolder.

--- FRHED/PreLink.bat
+++ FRHED/PreLink.bat
@@ -7,6 +7,6 @@ cscript CreateMasterPotFile.vbs
 cscript UpdatePoFilesFromPotFile.vbs
 cd ..
 rc /fo%1\lang.res /i.. Languages\heksedit.rc
-mkdir %2\..\heksedit.lng
-link /DLL /NOENTRY /MACHINE:IX86 /OUT:%2\..\heksedit.lng\heksedit.lng %1\lang.res
-copy Languages\*.po %2\..\heksedit.lng
+mkdir %2\..\Languages
+link /DLL /NOENTRY /MACHINE:IX86 /OUT:%2\..\Languages\heksedit.lng %1\lang.res
+copy Languages\*.po %2\..\Languages
