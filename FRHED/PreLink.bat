b002b225dc4f0fb1fe408e19d19619fec8483317
Kimmo Varis <kimmov@gmail.com>
2009-04-06 15:14:09 +0000

    Move Frhed executable PO files to new place /Translations/Frhed. Translations folder will be the folder where all translation files are placed.

--- FRHED/PreLink.bat
+++ FRHED/PreLink.bat
@@ -2,11 +2,11 @@ cd
 echo %0
 echo $(IntDir) = %1
 echo $(TargetPath) = %2
-cd Languages
+cd ..\Translations\Frhed
 cscript CreateMasterPotFile.vbs
 cscript UpdatePoFilesFromPotFile.vbs
-cd ..
-rc /fo%1\lang.res /i.. Languages\heksedit.rc
+cd ..\..\FRHED
+rc /fo%1\lang.res /i.. ..\Translations\Frhed\heksedit.rc
 mkdir %2\..\Languages
 link /DLL /NOENTRY /MACHINE:IX86 /OUT:%2\..\Languages\heksedit.lng %1\lang.res
-copy Languages\*.po %2\..\Languages
+copy ..\Translations\Frhed\*.po %2\..\Languages
