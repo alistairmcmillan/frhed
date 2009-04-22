ccf07fec653fb277c1f401b8b83a7532f858f526
Kimmo Varis <kimmov@gmail.com>
2009-04-22 10:00:36 +0000

    Make regtools Unicode compatible.

--- FRHED/regtools.cpp
+++ FRHED/regtools.cpp
@@ -25,61 +25,69 @@
 
 #include "precomp.h"
 #include "regtools.h"
+
 /*Recursively delete key for WinNT
 Don't use this under Win9x
 Don't use this to delete keys you know will/should have no subkeys
 This recursively deletes subkeys of the key and then
 returns the return value of RegDeleteKey(basekey,keynam)*/
-LONG RegDeleteWinNTKey(HKEY basekey, const char *keynam)
+LONG RegDeleteWinNTKey(HKEY basekey, LPCTSTR keynam)
 {
 	HKEY tmp;
 	LONG res;
 	res = RegOpenKeyEx(basekey, keynam, 0, KEY_READ, &tmp);
 	if (res == ERROR_SUCCESS)
 	{
-		char subkeynam[_MAX_PATH+1];
-		DWORD subkeylen = _MAX_PATH+1;
-		for (DWORD i = 0;; i++ ){//Delete subkeys for WinNT
+		TCHAR subkeynam[MAX_PATH + 1];
+		DWORD subkeylen = MAX_PATH + 1;
+		for (DWORD i = 0; ; i++)
+		{//Delete subkeys for WinNT
 			subkeynam[0] = 0;
-			res = RegEnumKey(tmp,i,subkeynam,subkeylen);
+			res = RegEnumKey(tmp,i,subkeynam, subkeylen);
 			if (res == ERROR_NO_MORE_ITEMS)
 				break;
 			DWORD numsub;
-			res = RegQueryInfoKey(tmp,NULL,NULL,NULL,&numsub,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+			res = RegQueryInfoKey(tmp, NULL, NULL, NULL, &numsub, NULL, NULL,
+					NULL, NULL, NULL, NULL, NULL);
 			if (res == ERROR_SUCCESS && numsub > 0)
 				RegDeleteWinNTKey(tmp, subkeynam);//Recursively delete
 			RegDeleteKey(tmp, subkeynam);
 		}
 		RegCloseKey(tmp);
 	}
-	return RegDeleteKey(basekey,keynam);
+	return RegDeleteKey(basekey, keynam);
 }
 
-LONG RegCopyValues(HKEY src, const char *skey, HKEY dst, const char *dkey)
+LONG RegCopyValues(HKEY src, LPCTSTR skey, HKEY dst, LPCTSTR dkey)
 {
 	HKEY sk,dk;
 	LONG res;
 
-	res = RegCreateKey(dst,dkey,&dk);
-	if(res==ERROR_SUCCESS)res = RegOpenKeyEx(src,skey,0,KEY_READ,&sk);
-	else RegCloseKey(dk);
+	res = RegCreateKey(dst, dkey, &dk);
+	if (res == ERROR_SUCCESS)
+		res = RegOpenKeyEx(src, skey, 0, KEY_READ, &sk);
+	else
+		RegCloseKey(dk);
 
-	if(res==ERROR_SUCCESS){
-		char valnam[_MAX_PATH+1];
+	if (res == ERROR_SUCCESS)
+	{
+		TCHAR valnam[MAX_PATH + 1];
 		DWORD valnamsize,typ;
-		char valbuf[_MAX_PATH+1];
+		TCHAR valbuf[MAX_PATH + 1];
 		DWORD valbufsize;
-		for(DWORD i = 0;;i++){
-			valnamsize = valbufsize = _MAX_PATH+1;
-			valbuf[0]=valnam[0]=0;
-			res = RegEnumValue(sk,i,valnam,&valnamsize,0,&typ,(BYTE*) valbuf,&valbufsize);
-			if(ERROR_NO_MORE_ITEMS==res)break;
-			if(res==ERROR_SUCCESS)
-				RegSetValueEx(dk,valnam,0,typ,(BYTE*)valbuf,valbufsize);
+		for (DWORD i = 0; ;i++)
+		{
+			valnamsize = valbufsize = MAX_PATH + 1;
+			valbuf[0] = valnam[0] = 0;
+			res = RegEnumValue(sk, i, valnam, &valnamsize, 0, &typ,
+					(BYTE*) valbuf, &valbufsize);
+			if (ERROR_NO_MORE_ITEMS == res)
+				break;
+			if (res == ERROR_SUCCESS)
+				RegSetValueEx(dk, valnam, 0, typ, (BYTE*)valbuf, valbufsize);
 		}
 		RegCloseKey(dk);
 		RegCloseKey(sk);
 	}
 	return res;
 }
-
