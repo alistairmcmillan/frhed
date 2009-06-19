c793f5c17de5e2a86feeaa707d5699a829cf06b9
Tim Gerundt <tim@gerundt.de>
2009-06-19 16:55:24 +0000

    Web: Init translations pages - Add list of languages to translations index page

--- Web/translations/index.php
+++ Web/translations/index.php
@@ -1,5 +1,6 @@
 <?php
   include('../engine/page.inc');
+  include('translations.inc');
 
   $page = new Page;
   $page->addRssFeed('status_branch_rss.php', 'Frhed Translations Status (Stable Branch)');
@@ -9,7 +10,21 @@
 ?>
 <h2>Translations</h2>
 <p>We currently have Frhed translated into the languages listed below:</p>
-<p>...</p>
+<ul>
+<?php
+  try {
+    $status = New TranslationsStatus('status_trunk.xml');
+    
+    $languages = $status->getLanguagesArray();
+    foreach ($languages as $language) { //for all languages...
+      print("  <li>" . $language . "</li>\n");
+    }
+  }
+  catch (Exception $ex) { //If problems with translations status...
+    print("  <li>English</li>\n");
+  }
+?>
+</ul>
 <h3><a name="status">Status</a></h3>
 <p>The following two pages inform you about the translations status of the stable and developer version of Frhed:</p>
 <ul>
