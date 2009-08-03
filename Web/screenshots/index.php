d38009696dd93ebda918c2664a5bf1b5b56f80da
Tim Gerundt <tim@gerundt.de>
2009-08-03 14:26:25 +0000

    Web: Add printHeading(), printSubHeading() and printPara() functions to Page class

--- Web/screenshots/index.php
+++ Web/screenshots/index.php
@@ -2,10 +2,11 @@
   include('../engine/engine.inc');
   
   $page = new Page;
-  $page->printHead('Screenshots - Frhed');
+  $page->printHead(__('Screenshots - Frhed'));
+  
+  $page->printHeading(__('Screenshots'));
+  $page->printSubHeading(__('Main Window'));
 ?>
-<h2>Screenshots</h2>
-<h3>Main Window</h3>
 <a href="frhed_main_window.png" target="_blank"><img src="frhed_main_window_small.png" alt="Main Window" style="border:none;" /></a>
 <p>The main window from Frhed.</p>
 <?php
