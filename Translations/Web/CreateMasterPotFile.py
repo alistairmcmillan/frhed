1f097be0c351d9fc7f615b22495d1b06aacf473d
Tim Gerundt <tim@gerundt.de>
2009-08-09 12:22:49 +0000

    Web: Add main routine to CreateMasterPotFile.py script

--- Translations/Web/CreateMasterPotFile.py
+++ Translations/Web/CreateMasterPotFile.py
@@ -32,58 +32,63 @@ def walkPhpFiles(phpfiles, dirpath, itemnames):
             if str.lower(filename[1]) == '.php' or str.lower(filename[1]) == '.inc': #If a PHP file...
                 phpfiles.append(fullitempath)
 
-def getTranslationsFromPhpFile(php_file, translations):
+def getTranslationsFromPhpFile(filepath, translations):
     ''' Get the translations from a php file '''
-    rGettext = re.compile('_e?\([\'](.*?)[\']', re.IGNORECASE)
+    rGettext = re.compile('_e?\([\'"](.*?)[\'"]', re.IGNORECASE)
 
     i = 0
-    lines = open(php_file, 'r')
-    for line in lines: #For all lines...
+    phpfile = open(filepath, 'r')
+    for line in phpfile: #For all lines...
         i += 1
         tmps = rGettext.findall(line)
         if tmps: #If found a gettext function...
             for tmp in tmps: #For all translations...
                 translation = tmp
                 if translation in translations: #If the translation is already exists...
-                    translations[translation] += [(php_file, i)]
+                    translations[translation] += [(filepath, i)]
                 else: #If the translation is NOT already exists...
-                    translations[translation] = [(php_file, i)]
+                    translations[translation] = [(filepath, i)]
+    phpfile.close()
 
-#Main routine...
-translations = {}
-php_dir = abspath('../../Web')
-php_files = getPhpFiles(php_dir)
-for php_file in php_files: #For all php files...
-    getTranslationsFromPhpFile(php_file, translations)
-
-potfile = open('en-US.pot', 'w')
-potfile.write('# This file is part from Frhed <http://frhed.sf.net/>\n')
-potfile.write('# Released under the "GNU General Public License"\n')
-potfile.write('#\n')
-potfile.write('# ID line follows -- this is updated by SVN\n')
-potfile.write('# $' + 'Id' + '$\n')
-potfile.write('#\n')
-potfile.write('msgid ""\n')
-potfile.write('msgstr ""\n')
-potfile.write('"Project-Id-Version: Frhed\\n"\n')
-potfile.write('"Report-Msgid-Bugs-To: http://sourceforge.net/tracker/?atid=1100163&group_id=236485\\n"\n')
-potfile.write('"POT-Creation-Date: %s\\n"\n' % strftime('%Y-%m-%d %H:%M+0000'))
-potfile.write('"PO-Revision-Date: \\n"\n')
-potfile.write('"Last-Translator: \\n"\n')
-potfile.write('"Language-Team: English <frhed-support@lists.sourceforge.net>\\n"\n')
-potfile.write('"MIME-Version: 1.0\\n"\n')
-potfile.write('"Content-Type: text/plain; charset=utf-8\\n"\n')
-potfile.write('"Content-Transfer-Encoding: 8bit\\n"\n')
-potfile.write('"X-Poedit-Language: English\\n"\n')
-potfile.write('"X-Poedit-SourceCharset: utf-8\\n"\n')
-potfile.write('"X-Poedit-Basepath: ../../Web/\\n"\n')
-potfile.write('\n')
-for translation in translations: #For all translations...
-    references = translations[translation]
-    for reference in references: #For all references...
-        potfile.write('#: %s:%u\n' % (reference[0].replace(php_dir + sep, ''), reference[1]))
-    potfile.write('#, c-format\n')
-    potfile.write('msgid "%s"\n' % (translation.replace('"', '\\"')))
+def main():
+    translations = {}
+    php_dir = abspath('../../Web')
+    php_files = getPhpFiles(php_dir)
+    for php_file in php_files: #For all php files...
+        getTranslationsFromPhpFile(php_file, translations)
+    
+    potfile = open('en-US.pot', 'w')
+    potfile.write('# This file is part from Frhed <http://frhed.sf.net/>\n')
+    potfile.write('# Released under the "GNU General Public License"\n')
+    potfile.write('#\n')
+    potfile.write('# ID line follows -- this is updated by SVN\n')
+    potfile.write('# $' + 'Id' + '$\n')
+    potfile.write('#\n')
+    potfile.write('msgid ""\n')
     potfile.write('msgstr ""\n')
+    potfile.write('"Project-Id-Version: Frhed\\n"\n')
+    potfile.write('"Report-Msgid-Bugs-To: http://sourceforge.net/tracker/?atid=1100163&group_id=236485\\n"\n')
+    potfile.write('"POT-Creation-Date: %s\\n"\n' % strftime('%Y-%m-%d %H:%M+0000'))
+    potfile.write('"PO-Revision-Date: \\n"\n')
+    potfile.write('"Last-Translator: \\n"\n')
+    potfile.write('"Language-Team: English <frhed-support@lists.sourceforge.net>\\n"\n')
+    potfile.write('"MIME-Version: 1.0\\n"\n')
+    potfile.write('"Content-Type: text/plain; charset=utf-8\\n"\n')
+    potfile.write('"Content-Transfer-Encoding: 8bit\\n"\n')
+    potfile.write('"X-Poedit-Language: English\\n"\n')
+    potfile.write('"X-Poedit-SourceCharset: utf-8\\n"\n')
+    potfile.write('"X-Poedit-Basepath: ../../Web/\\n"\n')
     potfile.write('\n')
-potfile.close()
+    for translation in translations: #For all translations...
+        references = translations[translation]
+        for reference in references: #For all references...
+            potfile.write('#: %s:%u\n' % (reference[0].replace(php_dir + sep, ''), reference[1]))
+        potfile.write('#, c-format\n')
+        potfile.write('msgid "%s"\n' % (translation.replace('"', '\\"')))
+        potfile.write('msgstr ""\n')
+        potfile.write('\n')
+    potfile.close()
+
+# MAIN #
+if __name__ == "__main__":
+    main()
