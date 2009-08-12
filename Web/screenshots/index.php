c37f5b23f3592fdd2430cc9828ed222739e2e509
Tim Gerundt <tim@gerundt.de>
2009-08-12 18:02:04 +0000

    Web: Make screenshot page translateable

--- Web/screenshots/index.php
+++ Web/screenshots/index.php
@@ -3,12 +3,13 @@
   
   $page = new Page;
   $page->printHead(__('Screenshots'));
-  
   $page->printHeading(__('Screenshots'));
+  
   $page->printSubHeading(__('Main Window'));
 ?>
-<a href="frhed_main_window.png" target="_blank"><img src="frhed_main_window_small.png" alt="Main Window" style="border:none;" /></a>
-<p>The main window from Frhed.</p>
+<a href="frhed_main_window.png" target="_blank"><img src="frhed_main_window_small.png" alt="<?php __e('Main Window');?>" style="border:none;" /></a>
 <?php
+  $page->printPara(__('The main window from Frhed.'));
+  
   $page->printFoot();
 ?>
