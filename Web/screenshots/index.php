06b0d7cf6ae32e2e1c904bdae30a0737e428e41e
Tim Gerundt <tim@gerundt.de>
2010-11-07 16:09:30 +0000

    Web: Move screenshots to "sites" directory

--- Web/screenshots/index.php
+++ Web/screenshots/index.php
@@ -7,7 +7,7 @@
   
   $page->printSubHeading(__('Main Window'));
 ?>
-<a href="frhed_main_window.png" target="_blank"><img src="frhed_main_window_small.png" alt="<?php __e('Main Window');?>" style="border:none;" /></a>
+<a href="/sites/screenshots/frhed_main_window.png" target="_blank"><img src="/sites/screenshots/frhed_main_window_small.png" alt="<?php __e('Main Window');?>" style="border:none;" /></a>
 <?php
   $page->printPara(__('The main window from Frhed.'));
   
